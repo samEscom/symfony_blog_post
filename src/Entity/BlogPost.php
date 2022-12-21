9589e3c112e28d46bf1d2c0f1c200e65a23c317f
Samuel Chavez <sa5m.escom@gmail.com>
2022-12-21 00:07:51 -0600

    modificar migracion

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -26,6 +26,9 @@ class BlogPost
     #[ORM\Column(length: 255)]
     private ?string $author = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -75,6 +78,18 @@ class BlogPost
     public function setAuthor(string $author): self
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
